87710164b973f56afde34c853474e72cc0a67e6e
RdStudios <77551723+RdStudios9145@users.noreply.github.com>
2024-02-25 17:28:41 -0800

    Finished auton, still need Action function to work tho

--- index.js
+++ index.js
@@ -2,12 +2,25 @@ var data = {};
 var actions = [];
 var redos = [];
 
+const mode = {
+  "auton": 0,
+  "teleop": 1,
+};
+
+const action = {
+  "intake": 0,
+  "shoot": 1,
+  "proceed": 2,
+}
+
 const onSection = (id) => {
   document.body.setAttribute("data-section", id);
 }
 
 const swapColor = () => {
   var color = document.getElementById("match_color").innerText;
+
+  // Cursed. String for the color red is 15 chars long, so if it is longer, its blue. Switch colors
   document.getElementById("match_color").innerText = color.length > 15 ? "Team Color: Red" : "Team Color: Blue"
 }
 
@@ -17,14 +30,46 @@ const start = () => {
   data['team #'] = document.getElementById("team_number").value;
   data['match'] = document.querySelector('input[name="match"]:checked').value;
 
-  data['auton in wing'] = 0;
-  data['auton in center'] = 0;
+  data['auton intake'] = Array(8).fill(0);
   data['auton made amp'] = 0;
   data['auton missed amp'] = 0;
   data['auton made speaker'] = 0;
   data['auton missed speaker'] = 0;
 
   onSection(0);
+};
+
+const getAutonDepositData = () => {
+  const deposit = document.querySelector('input[name="adeposit"]:checked').value;
+  const make = document.querySelector('input[name="amake"]:checked').value;
+
+  return { "deposit": deposit, "make": make };
+}
+
+const getTeleopDepositData = () => {
+  const deposit = document.querySelector('input[name="tdeposit"]:checked').value;
+  const make = document.querySelector('input[name="tmake"]:checked').value;
+
+  return { "deposit": deposit, "make": make };
+}
+
+const Action = (Mode, Action, data) => {
+  console.log(Mode, Action, data);
+
+  if (Mode == mode.auton)  actionAuton(Action, data);
+  if (Mode == mode.teleop) actionTeleop(Action, data);
+};
+
+const actionAuton = (Action, data) => {
+  switch (Action) {
+    // Hi Rishay!
+  }
+}
+
+const actionTeleop = (Action, data) => {
+  switch (Action) {
+    // Hi Rishay!
+  }
 }
 
 const setup = () => {
@@ -59,7 +104,21 @@ const setup = () => {
     counter.append(decrement, value, increment);
   });
 
-  console.log(counts);
+  // console.log(counts);
+
+  const one_eight_buttons = document.getElementById("one_eight_buttons");
+
+  for (var i = 0; i < 8; i++) {
+    let t = i;
+
+    var button = document.createElement("button");
+    button.innerText = i + 1;
+    button.onclick = () => Action(mode.auton, action.intake, { "id": t });
+
+    var br = document.createElement("br");
+
+    one_eight_buttons.append(button, br);
+  }
 }
 
 setup();
